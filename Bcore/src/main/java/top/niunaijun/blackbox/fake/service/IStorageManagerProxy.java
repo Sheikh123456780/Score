9a1cb41a9e4753443e2880a5b99d14841ea4e3d6
Sheikh123456780 <149171222+Sheikh123456780@users.noreply.github.com>
2026-08-02 15:37:38 +0530

    Update IStorageManagerProxy.java

--- Bcore/src/main/java/top/niunaijun/blackbox/fake/service/IStorageManagerProxy.java
+++ Bcore/src/main/java/top/niunaijun/blackbox/fake/service/IStorageManagerProxy.java
@@ -10,6 +10,7 @@ import black.android.os.mount.BRIMountServiceStub;
 import black.android.os.storage.BRIStorageManagerStub;
 import top.niunaijun.blackbox.BlackBoxCore;
 import top.niunaijun.blackbox.app.BActivityThread;
+import top.niunaijun.blackbox.core.system.os.BStorageManagerService;
 import top.niunaijun.blackbox.fake.hook.BinderInvocationStub;
 import top.niunaijun.blackbox.fake.hook.MethodHook;
 import top.niunaijun.blackbox.fake.hook.ProxyMethod;
@@ -21,73 +22,73 @@ import top.niunaijun.blackbox.utils.compat.BuildCompat;
  */
 public class IStorageManagerProxy extends BinderInvocationStub {
 
-	public IStorageManagerProxy() {
-		super(BRServiceManager.get().getService("mount"));
-	}
+    public static final String TAG = "IStorageManagerProxy";
 
-	@Override
-	protected Object getWho() {
-		IInterface mount;
-		if (BuildCompat.isOreo()) {
-			mount = BRIStorageManagerStub.get().asInterface(BRServiceManager.get().getService("mount"));
-		} else {
-			mount = BRIMountServiceStub.get().asInterface(BRServiceManager.get().getService("mount"));
-		}
-		return mount;
-	}
+    public IStorageManagerProxy() {
+        super(BRServiceManager.get().getService("mount"));
+    }
 
-	@Override
-	protected void inject(Object baseInvocation, Object proxyInvocation) {
-		replaceSystemService("mount");
-	}
+    @Override
+    protected Object getWho() {
+        IInterface mount;
+        if (BuildCompat.isOreo()) {
+            mount = BRIStorageManagerStub.get().asInterface(BRServiceManager.get().getService("mount"));
+        } else {
+            mount = BRIMountServiceStub.get().asInterface(BRServiceManager.get().getService("mount"));
+        }
+        return mount;
+    }
 
-	@Override
-	public boolean isBadEnv() {
-		return false;
-	}
+    @Override
+    protected void inject(Object baseInvocation, Object proxyInvocation) {
+        replaceSystemService("mount");
+    }
 
-	@ProxyMethod("getVolumeList")
-	public static class GetVolumeList extends MethodHook {
-		@Override
-		protected Object hook(Object who, Method method, Object[] args) throws Throwable {
-			if (args == null) {
-				StorageVolume[] volumeList = VBoxCore.getBStorageManager().getVolumeList(BActivityThread.getBUid(), null, 0, BActivityThread.getUserId());
-				if (volumeList == null) {
-					return method.invoke(who, args);
-				}
-				return volumeList;
-			}
-			try {
-				int uid = Integer.parseInt(args[0] + "");
-				String packageName = (String) args[1];
-				int flags = Integer.parseInt(args[2] + "");
-				StorageVolume[] volumeList = VBoxCore.getBStorageManager().getVolumeList(uid, packageName, flags, BActivityThread.getUserId());
-				if (volumeList == null) {
-					return method.invoke(who, args);
-				}
-				return volumeList;
-			} catch (Throwable t) {
-				return method.invoke(who, args);
-			}
-		}
-	}
+    @Override
+    public boolean isBadEnv() {
+        return false;
+    }
 
-	@ProxyMethod("mkdirs")
-	public static class mkdirs extends MethodHook {
-		@Override
-		protected Object hook(Object who, Method method, Object[] args) throws Throwable {
-			return 0;
-		}
-	}
-	
-	private static int getFlags(Object arg) {
-		if (arg instanceof Integer) {
-			return (Integer) arg;
-		}
-		if (arg instanceof Long) {
-			return ((Long) arg).intValue();
-		}
-		return 0;
-	}
+    @ProxyMethod("getVolumeList")
+    public static class GetVolumeList extends MethodHook {
+        @Override
+        protected Object hook(Object who, Method method, Object[] args) throws Throwable {
+            try {
+                BStorageManagerService storageManager = BlackBoxCore.getBStorageManager();
+                if (args == null || args.length == 0) {
+                    StorageVolume[] volumeList = storageManager.getVolumeList(
+                        BActivityThread.getBUid(), null, 0, BActivityThread.getUserId());
+                    return volumeList != null ? volumeList : method.invoke(who, args);
+                }
+                int uid = Integer.parseInt(String.valueOf(args[0]));
+                String packageName = args.length > 1 ? (String) args[1] : null;
+                int flags = args.length > 2 ? getFlags(args[2]) : 0;
+                
+                StorageVolume[] volumeList = storageManager.getVolumeList(
+                    uid, packageName, flags, BActivityThread.getUserId());
+                return volumeList != null ? volumeList : method.invoke(who, args);
+            } catch (Throwable t) {
+                Slog.e(TAG, "getVolumeList error", t);
+                return method.invoke(who, args);
+            }
+        }
+    }
+
+    @ProxyMethod("mkdirs")
+    public static class mkdirs extends MethodHook {
+        @Override
+        protected Object hook(Object who, Method method, Object[] args) throws Throwable {
+            return 0;
+        }
+    }
+
+    private static int getFlags(Object arg) {
+        if (arg instanceof Integer) {
+            return (Integer) arg;
+        }
+        if (arg instanceof Long) {
+            return ((Long) arg).intValue();
+        }
+        return 0;
+    }
 }
-
