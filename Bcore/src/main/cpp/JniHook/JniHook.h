0342a24f9bd3fb4d427a508c11790a995bb4ff93
Sheikh123456780 <149171222+Sheikh123456780@users.noreply.github.com>
2026-08-06 18:58:12 +0530

    Update JniHook.h

--- Bcore/src/main/cpp/JniHook/JniHook.h
+++ Bcore/src/main/cpp/JniHook/JniHook.h
@@ -17,6 +17,7 @@ public:
     static void HookJniFun(JNIEnv *env, const char *class_name, const char *method_name, const char *sign, void * new_fun, void ** orig_fun,
                            bool is_static);
     static void HookJniFun(JNIEnv *env, jobject java_method, void *new_fun, void **orig_fun, bool is_static);
+    static bool isInitialized() { return HookEnv.is_initialized; }
 };
 
 #endif //VIRTUAL_APP_JNIHOOK_H
