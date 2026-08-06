85bed7eef386b07e9fd10c329fdfb71c4e554960
Sheikh123456780 <149171222+Sheikh123456780@users.noreply.github.com>
2026-08-06 18:57:21 +0530

    Update hidden_api.h

--- Bcore/src/main/cpp/hidden_api.h
+++ Bcore/src/main/cpp/hidden_api.h
@@ -1,9 +1,9 @@
-
 #ifndef BLACKBOX2_HIDDEN_API_H
 #define BLACKBOX2_HIDDEN_API_H
 
 #include "jni.h"
 
 bool disable_hidden_api(JNIEnv*);
+int get_android_api_level();
 
 #endif //BLACKBOX2_HIDDEN_API_H
