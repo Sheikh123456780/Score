0f4daee4213816ad4068d3eb5434891d687c8f39
Sheikh123456780 <149171222+Sheikh123456780@users.noreply.github.com>
2026-08-07 13:32:06 +0530

    Update IO.cpp

--- Bcore/src/main/cpp/IO.cpp
+++ Bcore/src/main/cpp/IO.cpp
@@ -4,75 +4,64 @@
 
 #include "IO.h"
 #include "Log.h"
+#include "BoxCore.h"
+#include <string>
+#include <vector>
+#include <mutex>
+#include <cstring>
 
-jmethodID getAbsolutePathMethodId;
+static jmethodID getAbsolutePathMethodId = nullptr;
 
-list<IO::RelocateInfo> relocate_rule;
+struct RelocateEntry {
+    std::string targetPath;
+    std::string relocatePath;
+};
 
-char *replace(const char *str, const char *src, const char *dst) {
-    const char *pos = str;
-    int count = 0;
-    while ((pos = strstr(pos, src))) {
-        count++;
-        pos += strlen(src);
-    }
-
-    size_t result_len = strlen(str) + (strlen(dst) - strlen(src)) * count + 1;
-    char *result = (char *) malloc(result_len);
-    memset(result, 0, result_len);
-
-    const char *left = str;
-    const char *right = nullptr;
-
-    while ((right = strstr(left, src))) {
-        strncat(result, left, right - left);
-        strcat(result, dst);
-        right += strlen(src);
-        left = right;
-    }
-    strcat(result, left);
-    return result;
-}
+static std::vector<RelocateEntry> s_relocate_rules;
+static std::mutex s_io_mutex;
+static thread_local char tls_path_buffer[4096];
 
 const char *IO::redirectPath(const char *__path) {
-    list<IO::RelocateInfo>::iterator iterator;
-    for (iterator = relocate_rule.begin(); iterator != relocate_rule.end(); ++iterator) {
-        IO::RelocateInfo info = *iterator;
-        if (strncmp(__path, info.targetPath, strlen(info.targetPath)) == 0 && !strstr(__path, "/SdCard/")) {
-            char *ret = replace(__path, info.targetPath, info.relocatePath);
-            // ALOGD("redirectPath %s  => %s", __path, ret);
-            return ret;
+    if (__path == nullptr) return nullptr;
+
+    std::lock_guard<std::mutex> lock(s_io_mutex);
+    for (const auto &rule : s_relocate_rules) {
+        if (strncmp(__path, rule.targetPath.c_str(), rule.targetPath.length()) == 0 && !strstr(__path, "/SdCard/")) {
+            std::string path_str(__path);
+            size_t pos = 0;
+            while ((pos = path_str.find(rule.targetPath, pos)) != std::string::npos) {
+                path_str.replace(pos, rule.targetPath.length(), rule.relocatePath);
+                pos += rule.relocatePath.length();
+            }
+
+            // Copy to thread-local buffer to avoid heap memory leak and race conditions
+            strncpy(tls_path_buffer, path_str.c_str(), sizeof(tls_path_buffer) - 1);
+            tls_path_buffer[sizeof(tls_path_buffer) - 1] = '\0';
+            return tls_path_buffer;
         }
     }
     return __path;
 }
 
 jstring IO::redirectPath(JNIEnv *env, jstring path) {
-//    const char * pathC = env->GetStringUTFChars(path, JNI_FALSE);
-//    const char *redirect = redirectPath(pathC);
-//    env->ReleaseStringUTFChars(path, pathC);
-//    return env->NewStringUTF(redirect);
     return BoxCore::redirectPathString(env, path);
 }
 
 jobject IO::redirectPath(JNIEnv *env, jobject path) {
-//    auto pathStr =
-//            reinterpret_cast<jstring>(env->CallObjectMethod(path, getAbsolutePathMethodId));
-//    jstring redirect = redirectPath(env, pathStr);
-//    jobject file = env->NewObject(fileClazz, fileNew, redirect);
-//    env->DeleteLocalRef(pathStr);
-//    env->DeleteLocalRef(redirect);
     return BoxCore::redirectPathFile(env, path);
 }
 
 void IO::addRule(const char *targetPath, const char *relocatePath) {
-    IO::RelocateInfo info{};
-    info.targetPath = targetPath;
-    info.relocatePath = relocatePath;
-    relocate_rule.push_back(info);
+    if (targetPath == nullptr || relocatePath == nullptr) return;
+
+    std::lock_guard<std::mutex> lock(s_io_mutex);
+    s_relocate_rules.push_back({std::string(targetPath), std::string(relocatePath)});
 }
 
 void IO::init(JNIEnv *env) {
     jclass tmpFile = env->FindClass("java/io/File");
-    getAbsolutePathMethodId = env->GetMethodID(tmpFile, "getAbsolutePath", "()Ljava/lang/String;");
+    if (tmpFile != nullptr) {
+        getAbsolutePathMethodId = env->GetMethodID(tmpFile, "getAbsolutePath", "()Ljava/lang/String;");
+        env->DeleteLocalRef(tmpFile);
+    }
 }
