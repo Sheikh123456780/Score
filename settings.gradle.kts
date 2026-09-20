1caaa16f7d1718a26d0914e7f13208cdfeeeb4a4
Sheikh123456780 <dev@score.com>
2026-09-20 19:34:38 +0530

    Upload fresh files from bbox

---
+++ settings.gradle.kts
@@ -0,0 +1,25 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        jcenter()
+        maven { url = uri("https://jitpack.io") }
+        flatDir {
+        dirs("libs")
+    }
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+        jcenter()
+    }
+}
+
+
+rootProject.name = "NewBlackbox-3.0"
+include(":Bcore")
